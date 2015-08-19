010bf67cf2edff7940c74715907e2e15c8e640a2
Tom Mortimer-Jones <tom@morty.co.uk>
2015-08-19 12:25:38 +0100

    Include the reference number in the search index

--- update_index.py
+++ update_index.py
@@ -5,14 +5,16 @@ import pyes
 es = pyes.ES('127.0.0.1:9200')
 
 try:
-    es.indices.delete_index('main-index')
-    es.indices.create_index('main-index')
+    #es.indices.delete_index('main-index')
+    #es.indices.create_index('main-index')
+    pass
 except:
     pass
 
 for notice in Notice.query.all():
     document = {}
     document['id'] = notice.id
+    document['ref_no'] = notice.ref_no
     document['title'] = notice.details.title
     document['description'] = notice.details.description
     es.index(document, 'main-index', 'notices', notice.id)
