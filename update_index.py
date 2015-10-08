07885da454ec4ef8f174c59c5bafad7db2dedab6
Tom Mortimer-Jones <tom@morty.co.uk>
2015-10-08 10:00:26 +0100

    Index location_path with path_hierarchy tokenizer

--- update_index.py
+++ update_index.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from contractfinder import Notice
+import time
 
 app = Flask(__name__)
 app.config.from_envvar('SETTINGS')
@@ -11,8 +12,11 @@ import pyes
 es = pyes.ES('127.0.0.1:9200')
 
 try:
-    #es.indices.delete_index(INDEX)
-    #es.indices.create_index(INDEX)
+
+    es.indices.delete_index(INDEX)
+    es.indices.create_index(INDEX)
+    time.sleep(5)
+    print "Created Index", INDEX
     pass
 except:
     pass
@@ -24,6 +28,16 @@ mapping = {
             'raw' : {
                 'type': 'string',
                 'index': 'not_analyzed'
+            }
+        }
+    },
+    'location_path': {
+        'type': 'string',
+        'index': 'not_analyzed',
+        'fields': {
+            'tree': {
+                'type': 'string',
+                'analyzer': 'paths'
             }
         }
     },
@@ -47,6 +61,20 @@ mapping = {
     },
 }
 
+settings = {
+    'analysis': {
+        'analyzer': {
+            'paths': {
+                'tokenizer': 'path_hierarchy'
+            }
+        }
+    }
+}
+
+es.indices.close_index(INDEX)
+es.indices.update_settings(INDEX, settings)
+es.indices.open_index(INDEX)
+
 es.indices.put_mapping('notices', {'properties': mapping}, [INDEX])
 
 for notice in Notice.query.all():
@@ -59,6 +87,7 @@ for notice in Notice.query.all():
     document['contact_address'] = notice.details.contact_address
     if notice.location:
         document['location_name'] = notice.location.name
+        document['location_path'] = notice.location.location_path()
     if notice.award and notice.award.details:
         document['business_name'] = notice.award.details.business_name
         document['business_address'] = notice.award.details.business_address
